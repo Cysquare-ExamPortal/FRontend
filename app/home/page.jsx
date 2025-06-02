4c042c65af98680e7c2c9785395e7d1a002e26bd
NATTETIVENKATESWARLU <n.venkateswarlu180@gmail.com>
2025-06-02 20:42:37 +0530

    navmodify

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,7 +5,7 @@ const page = () => {
     <div>
       <h1>hello home  Page first one</h1>
       <p>show output</p>
-      <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore illo nihil expedita blanditiis ipsum. Dolorum cum voluptate sunt dolores soluta molestias fugit voluptatibus ipsa quam? Maiores ab tenetur necessitatibus consequuntur?</h2>
+      <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore illo nihil expedita blanditiis ipsum. ?</h2>
     </div>
   )
 }
