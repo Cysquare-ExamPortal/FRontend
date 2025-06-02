cf5cbf6ac9c01557f58953c9302c52a44d729682
NATTETIVENKATESWARLU <n.venkateswarlu180@gmail.com>
2025-06-02 20:36:42 +0530

    home add lorem

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
     <div>
       <h1>hello home  Page first one</h1>
       <p>show output</p>
+      <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore illo nihil expedita blanditiis ipsum. Dolorum cum voluptate sunt dolores soluta molestias fugit voluptatibus ipsa quam? Maiores ab tenetur necessitatibus consequuntur?</h2>
     </div>
   )
 }
