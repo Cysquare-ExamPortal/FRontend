8b379ebc72198df5319f6d3414de0dd9eac8fad8
BATHULA PRAKASH <152945490+PrakashBathula88@users.noreply.github.com>
2025-07-01 19:33:47 +0530

    Update page.jsx

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,6 +5,7 @@ const page = () => {
     <div>
       <h1>hello home  Page first one</h1>
       <p>show output</p>
+       <p>prakash changed something</p>
       <p>right here</p>
       <p>Prakash Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores atque eum consequatur quod possimus, officia magnam sequi commodi at, sunt ut fugiat eius sit itaque mollitia sapiente dolorum! Voluptates.</p>
       <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore illo nihil expedita blanditiis ipsum. ?</h2>
