ae1febb3f0db391b1c9d2b51e74b4fb067fca41f
tharun m <tharunmaddu1@outlook.com>
2025-07-02 17:39:27 +0530

    <p> tag

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -4,8 +4,9 @@ const page = () => {
   return (
     <div>
       <h1>hello home  Page first one</h1>
+      <p>tharun login page</p>
       <p>show output</p>
-       <p>prakash changed something</p>
+       <p>prakash changedsomething</p>
       <p>right here</p>
       <p>Prakash Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolores atque eum consequatur quod possimus, officia magnam sequi commodi at, sunt ut fugiat eius sit itaque mollitia sapiente dolorum! Voluptates.</p>
       <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore illo nihil expedita blanditiis ipsum. ?</h2>
